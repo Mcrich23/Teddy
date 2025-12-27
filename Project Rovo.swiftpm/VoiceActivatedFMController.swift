c7508c482a62877c7859d7e163993c5ae04e8e8c
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2025-12-27 01:51:32 -0800

    file cleanup

--- Project Rovo.swiftpm/VoiceActivatedFMController.swift
+++ Project Rovo.swiftpm/VoiceActivatedFMController.swift
@@ -132,3 +132,52 @@ enum FMErrors: Error {
     case noResponse
     case modelBusy
 }
+
+extension AttributedString {
+    init(styledMarkdown markdownString: String) throws {
+        let newLine = AttributedString("\n")
+        let markdownString = markdownString
+            .replacingOccurrences(of: "\n\n", with: "\u{2029}\u{2029}\n\n")
+        
+        var output = try AttributedString(
+            markdown: markdownString,
+            options: .init(
+                allowsExtendedAttributes: true,
+                interpretedSyntax: .full,
+                failurePolicy: .returnPartiallyParsedIfPossible
+            ),
+            baseURL: nil
+        )
+        
+        while let range = output.range(of: "\u{2029}\u{2029}") {
+            output[range] = newLine[newLine.startIndex..<newLine.endIndex]
+        }
+
+        for (intentBlock, intentRange) in output.runs[AttributeScopes.FoundationAttributes.PresentationIntentAttribute.self].reversed() {
+            guard let intentBlock = intentBlock else { continue }
+            for intent in intentBlock.components {
+                switch intent.kind {
+                case .header(level: let level):
+                    switch level {
+                    case 1:
+                        output[intentRange].font = .system(.title).bold()
+                    case 2:
+                        output[intentRange].font = .system(.title2).bold()
+                    case 3:
+                        output[intentRange].font = .system(.title3).bold()
+                    default:
+                        break
+                    }
+                default:
+                    break
+                }
+            }
+            
+            if intentRange.lowerBound != output.startIndex {
+                output.characters.insert(contentsOf: "\n", at: intentRange.lowerBound)
+            }
+        }
+
+        self = output
+    }
+}
